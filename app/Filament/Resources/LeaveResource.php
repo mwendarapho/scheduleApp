5f217c4c2e328d04146356aee80d8d0f887804a3
mwendarapho <mwendaraphael@gmail.com>
2023-11-14 00:27:44 +0300

    Leave resource

---
+++ app/Filament/Resources/LeaveResource.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\LeaveResource\Pages;
+use App\Filament\Resources\LeaveResource\RelationManagers;
+use App\Models\Leave;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class LeaveResource extends Resource
+{
+    protected static ?string $model = Leave::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('employee_id'),
+                Forms\Components\Select::make('leave_type')
+                    ->options([
+                        'vacation'=>'vacation',
+                        'sick_leave'=>'sick_leave',
+                        'personal_leave'=>'personal_leave',
+                    ]),
+                Forms\Components\DatePicker::make('start_date'),
+                Forms\Components\DatePicker::make('end_date'),
+                Forms\Components\TextInput::make('reason'),
+                Forms\Components\Select::make('status')
+                    ->options([
+                        'pending'=>'pending',
+                        'approved'=>'approved',
+                        'denied'=>'denied',
+                    ]),
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('employee.first_name')->label('First Name'),
+                Tables\Columns\TextColumn::make('employee.last_name')->label('Last Name'),
+                Tables\Columns\TextColumn::make('leave_type'),
+                Tables\Columns\TextColumn::make('start_date')->dateTime(),
+                Tables\Columns\TextColumn::make('end_date')->dateTime(),
+                Tables\Columns\TextColumn::make('reason'),
+                Tables\Columns\TextColumn::make('status'),
+
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListLeaves::route('/'),
+            'create' => Pages\CreateLeave::route('/create'),
+            'edit' => Pages\EditLeave::route('/{record}/edit'),
+        ];
+    }
+}
